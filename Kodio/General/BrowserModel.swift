afc87829ba89f07ad2a2d799184562a4caab6fc6
Nick Berendsen <58348935+Desbeers@users.noreply.github.com>
2022-11-04 16:57:49 +0100

    Faster Browser View
    'Release' is a lot faster than 'Debug' build btw

--- Kodio/General/BrowserModel.swift
+++ Kodio/General/BrowserModel.swift
@@ -32,7 +32,7 @@ class BrowserModel: ObservableObject {
     var library = Media()
     /// The current ``Router`` selection
     let router: Router
-    /// Te optional search query
+    /// The optional search query
     let query: String
     /// Init the model with the current ``Router``
     init(router: Router, query: String) {
@@ -73,25 +73,26 @@ extension BrowserModel {
     ///
     /// The browser library is based on songs; they are filtered first and then the rest is added
     func filterLibrary() {
+
+        let start = CFAbsoluteTimeGetCurrent()
+        
         /// Set the state
         state = state == .ready ? .ready : .loading
         /// Get the shared KodiConnector
         let kodi: KodiConnector = .shared
-        
-        /// Get all songs from the library
-        let songs = kodi.library.songs.sorted { $0.displayArtist < $1.displayArtist }
+
         /// Filter the songs
         switch router {
         case .recentlyAdded:
             library.songs = Array(
-                songs.sorted {
+                kodi.library.songs.sorted {
                         $0.dateAdded > $1.dateAdded
                     }
                     .prefix(100)
             )
         case .recentlyPlayed:
             library.songs = Array(
-                songs.filter {
+                kodi.library.songs.filter {
                     $0.playcount > 0
                 }.sorted {
                     $0.lastPlayed > $1.lastPlayed
@@ -100,7 +101,7 @@ extension BrowserModel {
             )
         case .mostPlayed:
             library.songs = Array(
-                songs.filter {
+                kodi.library.songs.filter {
                     $0.playcount > 0
                 }.sorted {
                     $0.playcount > $1.playcount
@@ -108,7 +109,7 @@ extension BrowserModel {
                     .prefix(100)
             )
         case .favorites:
-            library.songs = songs
+            library.songs = kodi.library.songs
                 .filter {
                     $0.userRating > 0
                 }
@@ -116,50 +117,70 @@ extension BrowserModel {
                     $0.userRating > $1.userRating
                 }
         case .search:
-            library.songs = songs.search(query)
+            library.songs = kodi.library.songs.search(query)
         default:
-            library.songs = songs
+            library.songs = kodi.library.songs
+                .sorted {
+                    $0.displayArtist < $1.displayArtist
+                }
         }
-        
         if library.songs.isEmpty {
             state = .empty
         } else {
-            
+
             /// All albums in the browser
-            let songAlbums = library.songs.map({ $0.albumID })
-            library.albums = kodi.library.albums.filter({songAlbums.contains($0.albumID)}).sorted { $0.displayArtist < $1.displayArtist }
+            let songAlbumIDs = Set(library.songs.map({ $0.albumID }))
+            library.albums = kodi.library.albums
+                .filter({songAlbumIDs.contains($0.albumID)})
+                .sorted { $0.displayArtist == $1.displayArtist ? $0.year < $1.year : $0.displayArtist < $1.displayArtist }
+            
             /// All artists in the browser
-            let songArtists = library.songs.flatMap({ $0.albumArtistID }).removingDuplicates()
-            library.artists = kodi.library.artists.filter({songArtists.contains($0.artistID)}).sorted { $0.sortByTitle < $1.sortByTitle }
+            let songArtistIDs = Set(library.songs.flatMap({ $0.albumArtistID }))
+            library.artists = kodi.library.artists.filter({songArtistIDs.contains($0.artistID)}).sorted { $0.sortByTitle < $1.sortByTitle }
+            
             /// All genres in the browser
-            let songGenres = library.songs.flatMap({ $0.genreID }).removingDuplicates()
-            library.genres = kodi.library.audioGenres.filter({songGenres.contains($0.genreID)})
+            let songGenreIDs = Set(library.songs.flatMap({ $0.genreID }))
+            library.genres = kodi.library.audioGenres.filter({songGenreIDs.contains($0.genreID)})
+            
             /// Filter the browser based on selection
             filterBrowser()
             /// Set the state
             state = .ready
+            let diff = CFAbsoluteTimeGetCurrent() - start
+            logger("Took \(diff) seconds")
         }
     }
     
     /// Filter the ``BrowserView`` based on the optional ``BrowserModel/selection-swift.property``
     func filterBrowser() {
+
         var artists = library.artists
         var albums = library.albums
         var songs = library.songs
+        
+        /// Filter by genre
         if let genre = selection.genre {
             songs = songs.filter({$0.genreID.contains(genre.genreID)})
             albums = albums.filter({$0.genre.contains(genre.title)})
-            let songArtists = songs.flatMap({ $0.albumArtistID }).removingDuplicates()
-            artists = artists.filter({songArtists.contains($0.artistID)}).sorted { $0.title < $1.title }
-        }
-        if let artist = selection.artist {
-            songs = songs.filter({$0.albumArtistID.contains(artist.artistID)}).sorted { $0.title < $1.title }
-            albums = albums.filter({$0.artistID.contains(artist.artistID)}).sorted { $0.year < $1.year }
+            let songArtistIDs = Set(songs.flatMap({ $0.albumArtistID }))
+            artists = artists.filter({songArtistIDs.contains($0.artistID)}).sorted { $0.title < $1.title }
         }
         
+        /// Filter by artist
+        if let artist = selection.artist {
+            songs = songs.filter({$0.albumArtistID.contains(artist.artistID)}).sorted { $0.title < $1.title }
+            albums = albums.filter({$0.artistID.contains(artist.artistID)})
+        }
+        
+        /// Filter by album
         if let album = selection.album {
             songs = songs.filter({$0.albumID == album.albumID}).sorted { $0.track < $1.track }
         }
-        items = Media(genres: library.genres, artists: artists, albums: albums, songs: songs)
+        /// Set the filtered items
+        items = Media(genres: library.genres,
+                      artists: artists,
+                      albums: albums,
+                      songs: songs
+        )
     }
 }
